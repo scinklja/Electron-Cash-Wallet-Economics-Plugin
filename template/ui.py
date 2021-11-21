443a90a9a1d68672c6518613f16498fa92248c48
Scinklja <insane.socks@gmail.com>
2021-11-21 13:25:25 +0100

    new rows

--- template/ui.py
+++ template/ui.py
@@ -29,12 +29,19 @@ class Ui(MyTreeWidget, MessageBoxMixin):
         w = self.parent
         c = Commands(w.config, w.wallet, w.network, lambda: set_json(True))
         total_invest = 0
+        total_received = 0
+        total_sent = 0
+
 
         for x in c.history():
             value = float(x["value"]) * 100000000
             timestamp = datetime.fromtimestamp(x["timestamp"])
             investment = float(self.parent.fx.historical_value_str(value, timestamp))
             total_invest = total_invest + investment
+            if investment > 0:
+                total_received = total_received + investment
+            else:
+                total_sent = total_sent + investment
 
         balance = float(c.history()[0]["balance"]) * 100000000
         balance_bch = float(c.history()[0]["balance"])
@@ -42,6 +49,10 @@ class Ui(MyTreeWidget, MessageBoxMixin):
         balance_usd = float(self.parent.fx.historical_value_str(balance, datetime.now()))
 
         profit_usd = balance_usd - total_invest
+
+
+
+
 
         item1=QTreeWidgetItem([
             _("Current balance"),
@@ -51,6 +62,18 @@ class Ui(MyTreeWidget, MessageBoxMixin):
 
         item2 = QTreeWidgetItem([
             _("Profit"),
-            _(str(balance_usd)),
+            _(str(profit_usd)),
             _(str(balance_bch))])
         self.addTopLevelItem(item2)
+
+        item3 = QTreeWidgetItem([
+            _("Total received"),
+            _(str(total_received)),
+            _(str(balance_bch))])
+        self.addTopLevelItem(item3)
+
+        item4 = QTreeWidgetItem([
+            _("Total sent"),
+            _(str(total_sent)),
+            _(str(balance_bch))])
+        self.addTopLevelItem(item4)
